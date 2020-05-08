93d9f0e33de9eb23041ff2b762f68da45f4c7c33
Karen Lei <karenwinnielei@gmail.com>
2020-05-08 08:29:03 -0700

    complete header

--- components/Header/index.js
+++ components/Header/index.js
@@ -11,4 +11,30 @@
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
 
-function Header() {}
+const headContainer = document.querySelector('.header-container')
+console.log(headContainer)
+function Header() {
+    // const {date, title, temp} = headerAttr
+    const topHead = document.createElement('div')
+    const theDate = document.createElement('span')
+    const h1 = document.createElement('h1')
+    const theTemp = document.createElement('span')
+
+    headContainer.appendChild(topHead)
+    topHead.appendChild(theDate)
+    topHead.appendChild(h1)
+    topHead.appendChild(theTemp)
+
+    topHead.classList.add('header')
+    theDate.classList.add('date')
+    theTemp.classList.add('temp')
+
+    theDate.textContent = `SMARCH 28, 2019`
+    h1.textContent = `Lambda Times`
+    theTemp.textContent =  `98°`
+
+    console.log(topHead)
+    return topHead
+    
+}
+Header()
